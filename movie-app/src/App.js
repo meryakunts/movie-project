014838445fc1a75c4af994c1ccc736a7ca04640c
Mery <meryakunts@gmail.com>
2022-04-02 18:15:11 +0400

    setup initial project and added sign in component

---
+++ movie-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInComponent from "./components/SignInComponent"
+
+function App() {
+  return (
+    <div>
+      <SignInComponent/>
+    </div>
+  );
+}
+
+export default App;
