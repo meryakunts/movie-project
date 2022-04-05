38f65e241c8c59724c72722beb12a2d58c9452c7
Nayrie Sarkissian <nayrie.sarkissian@gmail.com>
2022-04-05 21:47:07 +0400

    Initial setup of dataStore branch.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import SignInComponent from "./components/SignInComponent";
 import SignUpComponent from "./components/SignUpComponent";
-import Home from "./components/Home"
+import Home from "./components/Home";
 import ForgotPasswordComponent from "./components/ForgotPasswordComponent";
 
 const firebaseConfig = {
@@ -17,7 +13,7 @@ const firebaseConfig = {
   projectId: "movie-project-26ffe",
   storageBucket: "movie-project-26ffe.appspot.com",
   messagingSenderId: "61136410009",
-  appId: "1:61136410009:web:87d80a8727cac9bd96ef08"
+  appId: "1:61136410009:web:87d80a8727cac9bd96ef08",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,12 +22,15 @@ const auth = getAuth(app);
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/home" component={Home}></Route>
-          <Route path="/signin" component={SignInComponent}></Route>
-          <Route path="/signup" component={SignUpComponent}></Route>
-          <Route path="/forgotpassword" component={ForgotPasswordComponent}></Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/home" component={Home}></Route>
+        <Route path="/signin" component={SignInComponent}></Route>
+        <Route path="/signup" component={SignUpComponent}></Route>
+        <Route
+          path="/forgotpassword"
+          component={ForgotPasswordComponent}
+        ></Route>
+      </Switch>
     </Router>
   );
 }
