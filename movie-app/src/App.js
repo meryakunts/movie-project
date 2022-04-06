c8092bf260b909b99e29dda5c6dcf14266df4323
Nayrie Sarkissian <nayrie.sarkissian@gmail.com>
2022-04-06 21:19:13 +0400

    Added Firestore data into application with a temporary List form.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import SignInComponent from "./components/SignInComponent";
 import SignUpComponent from "./components/SignUpComponent";
 import Home from "./components/Home";
 import ForgotPasswordComponent from "./components/ForgotPasswordComponent";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA50jGxyZVJEu0N_tDJh1SwdbEQzU3HyNs",
-  authDomain: "movie-project-26ffe.firebaseapp.com",
-  projectId: "movie-project-26ffe",
-  storageBucket: "movie-project-26ffe.appspot.com",
-  messagingSenderId: "61136410009",
-  appId: "1:61136410009:web:87d80a8727cac9bd96ef08",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+import db from "./firebase";
+import { onSnapshot, collection } from "firebase/firestore";
+// import FireStoreData from "./components/FireStoreData";
 
 function App() {
+  const [movies, setMovies] = useState("Loading");
+  const [shows, setShows] = useState("Loading");
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "movies"), (snapshot) =>
+        setMovies(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      ),
+    []
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "tvshows"), (snapshot) =>
+        setShows(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      ),
+    []
+  );
+
+  // console.log(shows);
   return (
     <Router>
       <Switch>
@@ -30,6 +39,39 @@ function App() {
           path="/forgotpassword"
           component={ForgotPasswordComponent}
         ></Route>
+        <Route path="/allshowing">
+          <div className="showingLists">
+            <h4>Movies:</h4>
+            <br></br>
+            {!movies[0].hasOwnProperty("id") ? (
+              <form>
+                <li key="movieLoading">Loading...</li>
+              </form>
+            ) : (
+              <form>
+                {movies.map((movie) => (
+                  <li key={movie.id}>
+                    Name: {movie.name} | Genre: {movie.genre} | Year:{" "}
+                    {movie.year}
+                  </li>
+                ))}
+              </form>
+            )}
+            <h4>Shows:</h4>
+            <br></br>
+            {!shows[0].hasOwnProperty("id") ? (
+              <form>
+                <li key="showLoading">Loading...</li>
+              </form>
+            ) : (
+              <form>
+                {shows.map((show) => (
+                  <li key={show.name}>{show.name}</li>
+                ))}
+              </form>
+            )}
+          </div>
+        </Route>
       </Switch>
     </Router>
   );
