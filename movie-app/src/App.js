0addf1f2ae57491c3a72601018726757c422a636
Nayrie Sarkissian <nayrie.sarkissian@gmail.com>
2022-04-12 22:20:12 +0400

    Updating dataStore with latest changes.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignInComponent from "./components/SignInComponent";
 import SignUpComponent from "./components/SignUpComponent";
