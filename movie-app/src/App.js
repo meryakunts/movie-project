0c8fbb3fd41d48d76b1bf31376f70271a7c7f934
Vahan Lalayan <vahan.lalayan.80@inbox.ru>
2022-04-14 16:48:51 +0400

    .

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,75 +4,16 @@ import SignInComponent from "./components/SignInComponent";
 import SignUpComponent from "./components/SignUpComponent";
 import Home from "./components/Home";
 import ForgotPasswordComponent from "./components/ForgotPasswordComponent";
-<<<<<<< HEAD
-import db from "./firebase";
-import { onSnapshot, collection } from "firebase/firestore";
-import Movies from './components/HomeMovies';
-import TopShows from './components/HomeTopshows';
-import TopMovies from './components/HomeTopMovies';
-import Watchlist from './components/HomeWatchlist';
-
-
-
-=======
 import AllShowing from "./components/AllShowing";
 import Movies from "./components/HomeMovies";
 import TopShows from "./components/HomeTopshows";
 import TopMovies from "./components/HomeTopMovies";
 import Watchlist from "./components/HomeWatchlist";
 import NestedList from "./components/sidebar/NestedList";
->>>>>>> b1cedd7aee533a491700628c383a12eebc286dd4
+
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-       
-      <Switch>
-          <Route path="/TopShows" component={TopShows}></Route>
-          <Route path="/TopMovies" component={TopMovies}></Route>
-          <Route path="/Watchlist" component={Watchlist}></Route>
-          <Route path="/movies" component={Movies}></Route>
-        <Route exact path="/home" component={Home}></Route>
-        <Route path="/signin" component={SignInComponent}></Route>
-        <Route path="/signup" component={SignUpComponent}></Route>
-        <Route path="/forgotpassword" component={ForgotPasswordComponent}
-        ></Route>
-        
-        <Route path="/allshowing">
-          <div className="showingLists">
-            <h4>Movies:</h4>
-            <br></br>
-            {!movies[0].hasOwnProperty("id") ? (
-              <form>
-                <li key="movieLoading">Loading...</li>
-              </form>
-            ) : (
-              <form>
-                {movies.map((movie) => (
-                  <li key={movie.id}>{movie.name}</li>
-                ))}
-              </form>
-            )}
-            <h4>Shows:</h4>
-            <br></br>
-            {!shows[0].hasOwnProperty("id") ? (
-              <form>
-                <li key="showLoading">Loading...</li>
-              </form>
-            ) : (
-              <form>
-                {shows.map((show) => (
-                  <li key={show.name}>{show.name}</li>
-                ))}
-              </form>
-            )}
-          </div>
-        </Route>
-      </Switch>
-
-    </Router>
-=======
     <>
       <Router>
         <Switch>
@@ -91,7 +32,7 @@ function App() {
         </Switch>
       </Router>
     </>
->>>>>>> b1cedd7aee533a491700628c383a12eebc286dd4
+
   );
 }
 
