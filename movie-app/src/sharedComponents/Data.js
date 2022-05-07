0eae07a11f12459a9801ac13a7a30cd79b7e7020
Nayrie Sarkissian <nayrie.sarkissian@gmail.com>
2022-05-07 12:59:48 +0400

    Pulling from Main, fixing pull issues.

--- movie-app/src/sharedComponents/Data.js
+++ movie-app/src/sharedComponents/Data.js
@@ -3,12 +3,58 @@ import "./styles.css";
 import { DataContext } from "../components/DataContext";
 import Item from "./Item";
 import CardComponent from "./CardComponent";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import noitem from "../Images/noitem.png";
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  noData: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: "1",
+    flexDirection: "column",
+    "& h2": {
+      color: "#4a6cca",
+      fontSize: "1.2rem",
+      fontWeight: "normal",
+    },
+  },
+}));
 
 function Data() {
+  const classes = useStyles();
   const data = useContext(DataContext);
   const { moviesData, showsData, searchString } = data;
   let allData = moviesData.concat(showsData);
   let isNotEmpty = false;
+
   for (let item of allData) {
     if (
       Object.values(item.name)
